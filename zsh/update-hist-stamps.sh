1c3155e19c691d94adef89791540c7cb6b671db6
Galacsh <galacsh.dev@gmail.com>
2024-05-01 01:42:07 +0900

    fix: simple regex is enough

--- zsh/update-hist-stamps.sh
+++ zsh/update-hist-stamps.sh
@@ -7,7 +7,7 @@ zshrc_file=$1
 
 info "Updating history timestamp format..."
 
-replace_this='#?[[:blank:]]*HIST_STAMPS=.*'
+replace_this='# HIST_STAMPS=.*'
 hist_stamps='HIST_STAMPS="yyyy-mm-dd"'
 
 sed -E -i '' "s/^${replace_this}/${hist_stamps}/" "${zshrc_file}"
