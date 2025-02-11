839a501037ae5edd1649533eef6a8c0d879a265e
Galacsh <galacsh.dev@gmail.com>
2025-02-11 14:16:26 +0900

    notice user to copy contents manually

--- zsh/copy-zshrc.sh
+++ zsh/copy-zshrc.sh
@@ -14,9 +14,9 @@ template_file="zshrc.zsh-template"
 
 if [[ -e "${zshrc_file}" ]]; then
   log "'.zshrc' already exists."
+  highlight "You need to manually copy the contents of the '~/zshrc.template' file to '~/.zshrc'."
   (cp -v -n "${template_file}" "${HOME}/${template_file}" 2>&1 | log) \
     || highlight "Maybe 'zshrc.zsh-template' already exists?"
 else
   cp -v -n "${template_file}" "${zshrc_file}" 2>&1 | highlight
 fi
-
